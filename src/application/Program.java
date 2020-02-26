15e1c78063fc47a6ed15b7f472f8b5ba7129fb75
Joao Silva <joaososilva1819@gmail.com>
2020-02-26 10:51:48 +0000

    Chess layer

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,14 @@
 package application;
 
+import Chess.ChessMatch;
 import boardgame.Position;
 
 public class Program {
 
     public static void main(String[]args){
 
-        Position pos = new Position(3,5);
-        System.out.print(pos);
-
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
 
     }
 }
